83d88d08d5e78995df60c97bb68a2f37d9dc4ecc
bartoszgamza <bartosz.gamza12@gmail.com>
2018-05-12 15:54:13 +0200

    added stats component

---
+++ src/components/Stats.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card v-for="plant in plants" :key="plant.key">
+      <v-card-title>
+        <h3>{{plant.name}}</h3>
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn flat>Reffiled</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    plants () {
+      return this.$store.getters.plants
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MyPlants from '@/components/MyPlants'
+import Stats from '@/components/Stats'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'My Plants',
       component: MyPlants
+    },
+    {
+      path: '/stats',
+      name: 'Stats',
+      component: Stats
     }
   ]
 })
