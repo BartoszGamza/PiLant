c2963714ad4826c6a63a8a89546ba43538c4bbcc
bartoszgamza <bartosz.gamza12@gmail.com>
2018-05-12 12:57:32 +0200

    card-expand done, problem with npm

--- src/components/Plant.vue
+++ src/components/Plant.vue
@@ -13,9 +13,15 @@
         <span class="grey--text">Current moist: 88% Current temperature: 20C Last watered 3 days ago</span>
         </div>
       </v-card-title>
-      <v-card-text>
-        
-       </v-card-text>
+      <v-card-text v-show="show">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi minima voluptatum corrupti, nemo eligendi fugiat quisquam repudiandae culpa pariatur magnam expedita maiores, aspernatur numquam, cum enim adipisci perferendis molestiae veniam.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn flat v-show="!show" @click.native="show = !show">Show more</v-btn>
+        <v-btn flat v-show="show" @click.native="show = !show">Back</v-btn>
+        <v-btn flat>Water me</v-btn>
+        <v-btn flat v-show="show">Edit</v-btn>
+      </v-card-actions>
      </v-card>
   </div>
 </template>
@@ -24,6 +30,7 @@
 export default {
   data () {
     return {
+      show: false
     }
   },
   computed: {
